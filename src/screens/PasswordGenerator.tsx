fdfcab55dbe1b12e920708b3102b41733bf5cff3
Antonio Alves <antonio.jpsalves@gmail.com>
2024-03-23 14:57:57 -0300

    feat: logos

--- src/screens/PasswordGenerator.tsx
+++ src/screens/PasswordGenerator.tsx
@@ -1,10 +1,15 @@
-import { VStack } from '@gluestack-ui/themed'
+import { Text, VStack } from '@gluestack-ui/themed'
 import { ScreenHeader } from '../components/ScreenHeader'
 
 export function PasswordGen() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Gerador de senhas" />
+      <VStack>
+        <Text color="$blueGray100">
+          Aperte no botão a baixo para gerar uma senha:
+        </Text>
+      </VStack>
     </VStack>
   )
 }
